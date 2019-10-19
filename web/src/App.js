c8204194319ada5edbe6a648960647bf896233cc
pacoito123 <pacoito@protonmail.com>
2019-10-18 19:24:26 -0500

    Initial setup and stuff.

---
+++ web/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min';
+
+const App = () => {
+	return (
+		<Fragment>
+			<h1 className='center'>voltec-inv</h1>
+		</Fragment>
+	);
+};
+
+export default App;
